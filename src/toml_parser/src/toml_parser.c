7f5407b93b62869a36a5457630bff96b6f79e8b8
陆巍 <willard.lu@outlook.com>
2023-11-27 11:35:34 +0800

    update

---
+++ src/toml_parser/src/toml_parser.c
@@ -0,0 +1,14 @@
+// @copyright Copyright 2023 Willard Lu
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+#include "toml_parser.h"
+
+/// @brief TOML解析器
+/// @param toml toml配置文件内容
+/// @return 成功：0，失败：1
+int TOMLParser(char * toml) {
+  return 0;
+}
